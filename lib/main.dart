0575822045618486d27ec35db55d72c19c266525
Aditya Thakur <adityachauhan9456923436@gmail.com>
2023-03-24 16:25:39 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const MyApp()); 
 }
 
 class MyApp extends StatelessWidget {
